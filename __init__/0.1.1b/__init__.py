dfc253f2e1c61f59235f3eb42b5f31c5821550cd
Friedrich Romstedt <friedrichromstedt@gmail.com>
2010-02-25 22:12:36 +0100

    Adding forgotten __init__.py.

---
+++ __init__/0.1.1b/__init__.py
@@ -0,0 +1,22 @@
+# Maintainer: Friedrich Romstedt <friedrichromstedt@gmail.com>
+# Copyright 2009 Friedrich Romstedt
+#    This file is part of diagram_cl.
+#
+#    diagram_cl is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    diagram_cl is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with diagram_cl.  If not, see <http://www.gnu.org/licenses/>.
+# $Last changed: 2009 Mar 22$
+# Developed since: 2008
+# File version: 0.1.1b
+has_mainloop=False
+from diagram import Diagram
+from layers import Layer,Layer2D,Layer3D
